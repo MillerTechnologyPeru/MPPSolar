da843644736aaf31fe1479f2a4ab218e03b1c171
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-04-04 10:44:47 -0500

    Added `MPPSolar.Connection`

--- Sources/MPPSolar/MPPSolar.swift
+++ Sources/MPPSolar/MPPSolar.swift
@@ -1,4 +1,136 @@
+//
+//  MPPSolar.swift
+//
+//
+//  Created by Alsey Coleman Miller on 4/4/20.
+//
+
+import Foundation
+
+#if os(macOS)
+import Darwin
+#elseif os(Linux) || os(Android)
+import Glibc
+#endif
+
+/// MPP Solar Device
 public final class MPPSolar {
     
+    // MARK: - Properties
     
+    public let connection: Connection
+    
+    // MARK: - Initialization
+    
+    public init(connection: Connection) {
+        self.connection = connection
+    }
+    
+    public convenience init?(path: String) {
+        if let connection = try? Connection.USB(path: path) {
+            self.init(connection: .usb(connection))
+        } else if let connection = try? Connection.Serial(path: path) {
+            self.init(connection: .serial(connection))
+        } else {
+            return nil
+        }
+    }
+    
+}
+
+// MARK: - Supporting Types
+
+public extension MPPSolar {
+    
+    enum Connection {
+        
+        /// USB Connection
+        case usb(USB)
+        
+        /// Serial Connection
+        case serial(Serial)
+    }
+}
+
+public protocol MPPSolarConnection {
+    
+    func send(_ data: Data) throws
+    
+    func recieve(_ size: Int) throws -> Data
+}
+
+public extension MPPSolar.Connection {
+    
+    /// MPP Solar USB Connection
+    final class USB: MPPSolarConnection {
+        
+        // MARK: - Properties
+        
+        public let path: String
+        
+        internal let fileDescriptor: Int32
+        
+        // MARK: Initialization
+        
+        deinit {
+            closeFD()
+        }
+        
+        public init(path: String = "/dev/hidraw0") throws {
+            let fileDescriptor = open(path, O_RDWR | O_NONBLOCK)
+            guard fileDescriptor != -1
+                else { throw POSIXError.fromErrno() }
+            self.path = path
+            self.fileDescriptor = fileDescriptor
+        }
+        
+        // MARK: Methods
+        
+        internal func closeFD() {
+            close(fileDescriptor)
+        }
+        
+        public func send(_ data: Data) throws {
+            assert(data.isEmpty == false, "Cannot write empty data")
+            let actualByteCount = data.withUnsafeBytes {
+                write(fileDescriptor, $0.baseAddress, $0.count)
+            }
+            guard actualByteCount >= 0
+                else { throw POSIXError.fromErrno() }
+            assert(data.count == actualByteCount, "Did not send \(data.count) bytes, instead \(actualByteCount)")
+        }
+        
+        public func recieve(_ size: Int = 256) throws -> Data {
+            return try readFD()
+        }
+        
+        internal func readFD(_ size: Int = 256) throws -> Data {
+            
+            var data = Data(repeating: 0x00, count: size)
+            let actualByteCount = data.withUnsafeMutableBytes {
+                read(fileDescriptor, $0.baseAddress, $0.count)
+            }
+            guard actualByteCount >= 0 else { throw POSIXError.fromErrno() }
+            if actualByteCount < size {
+                data.removeSubrange(actualByteCount ..< size)
+            }
+            assert(data.count == actualByteCount)
+            return data
+        }
+    }
+}
+
+public extension MPPSolar.Connection {
+    
+    /// MPP Solar Serial Connection
+    final class Serial {
+        
+        public let path: String
+        
+        public init(path: String, baudRate: UInt = 2400) throws {
+            self.path = path
+            // FIXME: Open Serial Device with SwiftSerial
+            fatalError("Not implemented")
+        }
+    }
 }
