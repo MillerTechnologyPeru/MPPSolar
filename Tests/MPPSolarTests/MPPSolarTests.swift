e32b41ef6b092c2a8980f406edd08216bd03b382
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-04-05 11:04:51 -0500

    Updated unit tests

--- Tests/MPPSolarTests/MPPSolarTests.swift
+++ Tests/MPPSolarTests/MPPSolarTests.swift
@@ -52,6 +52,7 @@ final class MPPSolarTests: XCTestCase {
         guard let (response, responseChecksum, expectedChecksum) = ProtocolID.Inquiry.Response.parse(responseData)
             else { XCTFail("Cannot parse"); return }
         XCTAssertEqual(response.protocolID, 30)
+        XCTAssertEqual(response.protocolID.description, "30")
         XCTAssertEqual(responseChecksum, expectedChecksum)
         XCTAssertEqual(responseChecksum, 0x9a0b)
     }
@@ -75,6 +76,7 @@ final class MPPSolarTests: XCTestCase {
         guard let (response, responseChecksum, expectedChecksum) = SerialNumber.Inquiry.Response.parse(responseData)
             else { XCTFail("Cannot parse"); return }
         XCTAssertEqual(response.serialNumber, "92631807100358")
+        XCTAssertEqual(response.serialNumber.description, "92631807100358")
         XCTAssertEqual(responseChecksum, expectedChecksum)
         XCTAssertEqual(responseChecksum, 0x97d9)
     }
@@ -86,17 +88,16 @@ final class MPPSolarTests: XCTestCase {
          Response: [40, 66, 231, 201, 13, 0, 0, 0]
          Mode: battery
          */
+                
+        let command = DeviceMode.Inquiry()
+        let commandData = Data([81, 77, 79, 68, 73, 193, 13])
+        XCTAssertEqual(command.data, commandData)
+        XCTAssertEqual(command.checksum, 0x49c1)
+        XCTAssertEqual(command.rawValue, "QMOD")
         
         let responseData = Data([40, 66, 231, 201, 13, 0, 0, 0])
-        let commandData = Data([81, 77, 79, 68, 73, 193, 13])
-        
-        let command = DeviceMode.Inquiry()
-        let commandChecksum = Checksum(calculate: Data(command.rawValue.utf8))
-        XCTAssertEqual(commandChecksum, 0x49c1)
-                
         guard let (response, responseChecksum, expectedChecksum) = DeviceMode.Inquiry.Response.parse(responseData)
             else { XCTFail("Cannot parse"); return }
-        
         XCTAssertEqual(response.mode, .battery)
         XCTAssertEqual(responseChecksum, expectedChecksum)
         XCTAssertEqual(responseChecksum, 0xE7C9)
@@ -104,11 +105,50 @@ final class MPPSolarTests: XCTestCase {
     
     func testGeneralStatus() {
         
-        let responseString = "001.0 00.0 228.0 60.0 0000 0000 000 334 23.75 000 051 0463 0000 000.0 23.83 00001 10010000 00 03 00000 000"
+        /**
+         Command: [81, 80, 73, 71, 83, 183, 169, 13]
+         Response: [40, 48, 48, 49, 46, 48, 32, 48, 48, 46, 48, 32, 50, 50, 57, 46, 48, 32, 54, 48, 46, 48, 32, 48, 48, 48, 48, 32, 48, 48, 48, 48, 32, 48, 48, 48, 32, 51, 53, 48, 32, 50, 52, 46, 56, 51, 32, 48, 48, 53, 32, 48, 52, 53, 32, 48, 52, 50, 50, 32, 48, 48, 48, 54, 32, 48, 50, 52, 46, 53, 32, 50, 52, 46, 56, 57, 32, 48, 48, 48, 48, 48, 32, 49, 48, 48, 49, 48, 49, 49, 48, 32, 48, 48, 32, 48, 51, 32, 48, 48, 49, 53, 55, 32, 48, 48, 48, 189, 115, 13, 0, 0]
+         General Status:
+         ▿ MPPSolar.GeneralStatus
+           - gridVoltage: 1.0
+           - gridFrequency: 0.0
+           - outputVoltage: 229.0
+           - outputFrequency: 60.0
+           - outputApparentPower: 0
+           - outputActivePower: 0
+           - outputLoadPercent: 0
+           - busVoltage: 350
+           - batteryVoltage: 24.83
+           - batteryChargingCurrent: 5
+           - batteryCapacity: 45
+           - inverterHeatSinkTemperature: 422
+           - solarInputCurrent: 6
+           - solarInputVoltage: 24.5
+           - batteryVoltageSCC: 24.89
+           - batteryDischargeCurrent: 0
+         */
         
-        guard let status = GeneralStatus(rawValue: responseString)
+        let command = GeneralStatus.Inquiry()
+        let commandData = Data([81, 80, 73, 71, 83, 183, 169, 13])
+        XCTAssertEqual(command.data, commandData)
+        XCTAssertEqual(command.checksum, 0xb7a9)
+        XCTAssertEqual(command.rawValue, "QPIGS")
+        
+        let responseData = Data([40, 48, 48, 49, 46, 48, 32, 48, 48, 46, 48, 32, 50, 50, 57, 46, 48, 32, 54, 48, 46, 48, 32, 48, 48, 48, 48, 32, 48, 48, 48, 48, 32, 48, 48, 48, 32, 51, 53, 48, 32, 50, 52, 46, 56, 51, 32, 48, 48, 53, 32, 48, 52, 53, 32, 48, 52, 50, 50, 32, 48, 48, 48, 54, 32, 48, 50, 52, 46, 53, 32, 50, 52, 46, 56, 57, 32, 48, 48, 48, 48, 48, 32, 49, 48, 48, 49, 48, 49, 49, 48, 32, 48, 48, 32, 48, 51, 32, 48, 48, 49, 53, 55, 32, 48, 48, 48, 189, 115, 13, 0, 0])
+        guard let (response, responseChecksum, expectedChecksum) = GeneralStatus.Inquiry.Response.parse(responseData)
             else { XCTFail("Cannot parse"); return }
+        XCTAssertEqual(responseChecksum, expectedChecksum)
+        XCTAssertEqual(responseChecksum, 0xBD73)
+        XCTAssertEqual(response.gridVoltage, 1.0)
+        XCTAssertEqual(response.gridFrequency, 0.0)
+        XCTAssertEqual(response.outputVoltage, 229.0)
+        XCTAssertEqual(response.outputFrequency, 60.0)
+        XCTAssertEqual(response.outputApparentPower, 0)
+        XCTAssertEqual(response.outputActivePower, 0)
+        XCTAssertEqual(response.outputLoadPercent, 0)
+        XCTAssertEqual(response.busVoltage, 350)
+        XCTAssertEqual(response.batteryVoltage, 24.83)
         
-        dump(status)
+        dump(response)
     }
 }
