e70421868de7bd160823ce1308a1801f3aa9f9db
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-04-04 20:50:51 -0500

    Updated Response

--- Sources/MPPSolar/Response.swift
+++ Sources/MPPSolar/Response.swift
@@ -10,7 +10,12 @@ import Foundation
 /// MPP Solar Response
 public protocol ResponseProtocol {
     
-    init?(data: Data)
+    init?(rawValue: String)
+}
+
+internal extension ResponseProtocol {
+    
+    static var prefix: String { return "(" }
 }
 
 internal extension ResponseProtocol {
@@ -18,13 +23,15 @@ internal extension ResponseProtocol {
     static func parse(_ data: Data) -> (Self, Checksum, Checksum)? {
         guard data.count > 3
             else { return nil }
-        let prefix = data.prefix(while: { $0 != "\r".utf8.first })
-        let responseData = prefix.subdataNoCopy(in: 0 ..< prefix.count - 2)
-        let checksumData = prefix.subdataNoCopy(in: prefix.count - 2 ..< prefix.count)
-        guard let response = Self.init(data: responseData),
+        let responseBodyData = data.prefix(while: { $0 != "\r".utf8.first })
+        let responseData = responseBodyData.subdataNoCopy(in: 1 ..< responseBodyData.count - 2)
+        let checksumData = responseBodyData.subdataNoCopy(in: responseBodyData.count - 2 ..< responseBodyData.count)
+        guard responseBodyData[0] == prefix.utf8.first,
+            let responseString = String(data: responseData, encoding: .utf8),
+            let response = Self.init(rawValue: responseString),
             let checksum = Checksum(data: checksumData)
             else { return nil }
-        let expectedChecksum = Checksum(calculate: responseData)
+        let expectedChecksum = Checksum(calculate: responseBodyData.subdataNoCopy(in: 0 ..< responseBodyData.count - 2))
         return (response, checksum, expectedChecksum)
     }
 }
