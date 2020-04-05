8a58e13d6900a04a2a9764c270fbed5a46c60535
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-04-04 21:50:48 -0500

    Updated unit tests

--- Tests/MPPSolarTests/MPPSolarTests.swift
+++ Tests/MPPSolarTests/MPPSolarTests.swift
@@ -6,6 +6,8 @@ final class MPPSolarTests: XCTestCase {
     
     static let allTests = [
         ("testChecksum", testChecksum),
+        ("testDeviceProtocolIDInquiry", testDeviceProtocolIDInquiry),
+        ("testDeviceMode", testDeviceMode)
     ]
     
     func testChecksum() {
@@ -19,6 +21,45 @@ final class MPPSolarTests: XCTestCase {
         let command = DeviceMode.Inquiry()
         let checksum = Checksum(calculate: Data(command.rawValue.utf8))
         XCTAssertEqual(checksum, 0x49c1)
+        XCTAssertEqual(checksum.description, "0x49C1")
+    }
+    
+    func testDeviceProtocolIDInquiry() {
+        
+        /**
+         Sent [81, 80, 73, 190, 172, 13]
+         Recieved [40, 80, 73, 51, 48, 154, 11, 13]
+         Protocol ID: 30
+         */
+        
+        let commandData = Data([81, 80, 73, 190, 172, 13])
+        XCTAssertEqual(commandData, ProtocolID.Inquiry().data)
+        
+        let responseData = Data([40, 80, 73, 51, 48, 154, 11, 13])
+        guard let (response, responseChecksum, expectedChecksum) = ProtocolID.Inquiry.Response.parse(responseData)
+            else { XCTFail("Cannot parse"); return }
+        XCTAssertEqual(response.protocolID, 30)
+        XCTAssertEqual(responseChecksum, expectedChecksum)
+        XCTAssertEqual(responseChecksum, 0x9a0b)
+    }
+    
+    func testDeviceSerialNumberInquiry() {
+        
+        /**
+         Sent [81, 73, 68, 214, 234, 13]
+         Recieved [40, 57, 50, 54, 51, 49, 56, 48]
+         Serial Number: 92631
+         */
+        
+        let commandData = Data([81, 73, 68, 214, 234, 13])
+        XCTAssertEqual(commandData, SerialNumber.Inquiry().data)
+        
+        let responseData = Data([40, 57, 50, 54, 51, 49, 56, 48])
+        guard let (response, responseChecksum, expectedChecksum) = SerialNumber.Inquiry.Response.parse(responseData)
+            else { XCTFail("Cannot parse"); return }
+        XCTAssertEqual(response.serialNumber, "92631807100358")
+        XCTAssertEqual(responseChecksum, expectedChecksum)
+        XCTAssertEqual(responseChecksum, 0x97d9)
     }
     
     func testDeviceMode() {
