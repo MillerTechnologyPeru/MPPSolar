8eb2b586fd4d5263b1624c696fc571e9994249f0
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-02-27 02:47:06 -0800

    Add `MPPSolarSingleValueDecodingContainer`

--- Sources/MPPSolar/Decoder.swift
+++ Sources/MPPSolar/Decoder.swift
@@ -49,7 +49,7 @@ internal final class MPPSolarDecoder: Decoder {
     
     func singleValueContainer() throws -> SingleValueDecodingContainer {
         log?("Requested single value container for path \"\(codingPath.path)\"")
-        fatalError()
+        return MPPSolarSingleValueDecodingContainer(referencing: self)
     }
 }
 
@@ -282,5 +282,92 @@ internal struct MPPSolarKeyedDecodingContainer <K: CodingKey> : KeyedDecodingCon
         defer { self.decoder.codingPath.removeLast() }
         self.decoder.log?("Will read \(T.self) at path \"\(decoder.codingPath.path)\"")
         return try self.decoder.readNumeric(T.self)
+    }
+}
+
+// MARK: - SingleValueDecodingContainer
+
+internal struct MPPSolarSingleValueDecodingContainer: SingleValueDecodingContainer {
+    
+    // MARK: Properties
+    
+    /// A reference to the decoder we're reading from.
+    let decoder: MPPSolarDecoder
+    
+    /// The path of coding keys taken to get to this point in decoding.
+    let codingPath: [CodingKey]
+    
+    // MARK: Initialization
+    
+    /// Initializes `self` by referencing the given decoder and container.
+    init(referencing decoder: MPPSolarDecoder) {
+        self.decoder = decoder
+        self.codingPath = decoder.codingPath
+    }
+    
+    // MARK: SingleValueDecodingContainer
+    
+    func decodeNil() -> Bool {
+        return false // FIXME: Decode nil
+    }
+    
+    func decode(_ type: Bool.Type) throws -> Bool {
+        return try decoder.readBool()
+    }
+    
+    func decode(_ type: Int.Type) throws -> Int {
+        try decoder.readNumeric(type)
+    }
+    
+    func decode(_ type: Int8.Type) throws -> Int8 {
+        try decoder.readNumeric(type)
+    }
+    
+    func decode(_ type: Int16.Type) throws -> Int16 {
+        try decoder.readNumeric(type)
+    }
+    
+    func decode(_ type: Int32.Type) throws -> Int32 {
+        try decoder.readNumeric(type)
+    }
+    
+    func decode(_ type: Int64.Type) throws -> Int64 {
+        try decoder.readNumeric(type)
+    }
+    
+    func decode(_ type: UInt.Type) throws -> UInt {
+        try decoder.readNumeric(type)
+    }
+    
+    func decode(_ type: UInt8.Type) throws -> UInt8 {
+        try decoder.readNumeric(type)
+    }
+    
+    func decode(_ type: UInt16.Type) throws -> UInt16 {
+        return try decoder.readNumeric(UInt16.self)
+    }
+    
+    func decode(_ type: UInt32.Type) throws -> UInt32 {
+        try decoder.readNumeric(type)
+    }
+    
+    func decode(_ type: UInt64.Type) throws -> UInt64 {
+        try decoder.readNumeric(type)
+    }
+    
+    func decode(_ type: Float.Type) throws -> Float {
+        try decoder.readFloat()
+    }
+    
+    func decode(_ type: Double.Type) throws -> Double {
+        try decoder.readDouble()
+    }
+    
+    func decode(_ type: String.Type) throws -> String {
+        return try decoder.readString()
+    }
+    
+    func decode <T : Decodable> (_ type: T.Type) throws -> T {
+        return try decoder.readDecodable(type)
     }
 }
